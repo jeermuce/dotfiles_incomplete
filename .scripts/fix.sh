573a4ef6faadd025b1577a185e5602432c5fe89c
jeermuce <ernesto_mc.dev@proton.me>
2024-06-27 02:15:36 -0600

    fix

--- .scripts/fix.sh
+++ .scripts/fix.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-#check if oxlint and eslint are installed
-if ! command -v oxlint &> /dev/null; then
-#if currently at ~, install oxlint globally
-    echo "{GREEN}INSTALLING OXLINT@LATEST LOCALLY AS DEV DEPENDENCY{NC}"
-    bun install oxlint@latest  -D
-    
-    
-fi
 
-if ! command -v eslint &> /dev/null; then
-#if currently at ~, install eslint globally
-    echo "{GREEN}INSTALLING ESLINT@LATEST LOCALLY AS DEV DEPENDENCY{NC}"
-    bun install eslint@latest  -D
-    echo "{GREEN}INSTALLING ESLINT PLUGINS{NC}"
-    bun eslint --init
-
-fi
-
-bun oxlint . --fix && bun eslint . --fix
+bun oxlint --fix --jsdoc-plugin --jest-plugin --vitest-plugin --jsx-a11y-plugin --nextjs-plugin --react-perf-plugin && next lint && eslint . --fix
